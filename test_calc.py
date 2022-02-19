2a988fe2128e193ad4700a17333a195c63785d00
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-19 10:47:32 +0300

    created tests for the calc.py functions. Added employee file with its functions for testing

--- test_calc.py
+++ test_calc.py
@@ -6,7 +6,23 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         result = calc.add(20, 5)
         self.assertEqual(result, 25)
+    
+    def test_mutliple(self):
+        result = calc.multiply(20, 5)
+        self.assertEqual(result, 100)
+    
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(20, 5), 15)
+        self.assertEqual(calc.subtract(20, 10), 10)
+    
+    def test_divide(self):
+        self.assertEqual(calc.divide(20, 2), 10)
 
+        # testing exceptions using context manager
+        with self.assertRaises(ValueError):
+            calc.divide(20, 0)
 
 if __name__ == "__main__":
     unittest.main()
+
+# python3 -m unittest test_calc.py
